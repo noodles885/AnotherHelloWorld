f412de23269dede003f48a2a47a214e33ffbb8ba
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-09-18 09:28:15 -0300

    update

--- AnotherHelloWorld/Program.cs
+++ AnotherHelloWorld/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,57 @@ namespace AnotherHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int score = 0;
+            int health = 100;
+            int lives = 3;
+            int pointsgained;
+            int healthlost;
+
+            Console.WriteLine("Hud v1");
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key ");
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            Console.WriteLine("=====================");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("=====================");
+
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            
+            pointsgained = 250;
+            healthlost = 20;
+
+            score = score + pointsgained;
+            health = health - healthlost;
+
+            Console.WriteLine("=====================");
+
+            Console.WriteLine("In Battle the ");
+            Console.WriteLine("player gained " + pointsgained + " points.");
+            
+            Console.WriteLine("In Battle the ");
+            Console.WriteLine("player lost " + healthlost + " health.");
+
+            Console.WriteLine("=====================");
+
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            Console.WriteLine("=====================");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("=====================");
+
+
+
             Console.ReadKey();
         }
     }
